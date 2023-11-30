ffc0689783121720240c37d375190d13f4da939a
arthurcaixeta <104603222+arthurcaixeta@users.noreply.github.com>
2023-11-30 10:15:31 -0300

    Add files via upload

---
+++ PHP/config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbhost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'forum';
+
+    $conexao = new mysqli($dbhost,$dbUsername,$dbPassword,$dbName);
+
+   // if($conexao->connect_errno)
+   // {
+   //     echo "Erro";
+   //}
+   // else
+   //{
+   //    echo "Conexão efetuada com sucesso";
+   // }
+
+?>
